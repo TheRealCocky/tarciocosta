123b18bf2c2e1dd503d309879945237d21044245
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2025-07-18 15:02:57 +0100

    analticys react

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-
-// 👉 Importa e ativa o Vercel Analytics
-import { inject } from '@vercel/analytics';
-inject();
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <>
+            <App />
+            <Analytics />
+        </>
     </StrictMode>
 );
+
 
