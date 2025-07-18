c73374811fd04d6ab216270447c9419e32acfd7d
Euclides Baltazar <167758418+TheRealCocky@users.noreply.github.com>
2025-07-18 14:59:49 +0100

    put inseide de main page vercel analitycs

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+// 👉 Importa e ativa o Vercel Analytics
+import { inject } from '@vercel/analytics';
+inject();
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
+
